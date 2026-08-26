39a011fe6ef000b025d9c5b91b0cf18a4b03367a
meiyunchao <2511633874@qq.com>
2026-08-26 17:51:24 +0800

    init

--- apps/client/src/vite-env.d.ts
+++ apps/client/src/vite-env.d.ts
@@ -6,7 +6,7 @@ declare module "*.vue" {
   export default component;
 }
 
-declare module "@zhangxianjue/markdown-vue3" {
+declare module "@zhangxianjue/markdown-editor-v3" {
   const Markdown: any;
   export { Markdown };
 }
--- server/models/article.js
+++ server/models/article.js
@@ -1,16 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Article extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
+  class Article extends Model {}
   Article.init(
     {
       title: DataTypes.STRING,
